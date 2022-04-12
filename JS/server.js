30606cd58933e9577b2844435cb58bafb2efe18a
berryb0y <ab4537@gmail.com>
2022-04-11 23:55:00 -0500

    Initial commit

---
+++ JS/server.js
@@ -0,0 +1,42 @@
+const http = require("http"),
+    fs = require('fs'),
+    url = require('url');
+
+http.createServer((request, response) => {
+    let addr = parse.url,
+    q = url.parse(addr, true),
+    filePath = '';
+    // where we will store the file path
+
+
+    fs.appendFile('log.txt', 'URL: ' + addr + '/nTimestamp: ' + new 
+    Date() + '/n/n', (err) =>{
+        if (err){
+            console.log(err);
+        } else {
+            console.log('Added to log.');
+        }
+    });
+
+    if (q.pathname.includes('documentation')) {
+        filePath = (__dirname + '/documentation.html');
+    } else {
+        filePath = 'index.html';
+    }
+    // if documentation doesn't exist, users will be brought back to the homepage.
+    
+
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            throw err;
+        }
+    
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write(data);
+    response.end();
+    
+    });
+
+}).listen(8080);
+
+console.log('My first Node test server is running on Port 8080');
