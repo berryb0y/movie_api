1ab0902f8ebee9c10112e91fda81af64dc49f6bc
berryb0y <ab4537@gmail.com>
2022-04-13 00:45:35 -0500

    server fix

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require("http"),
     url = require('url');
 
 http.createServer((request, response) => {
-    let addr = parse.url,
+    let addr = request.url,
     q = url.parse(addr, true),
     filePath = '';
     // where we will store the file path
