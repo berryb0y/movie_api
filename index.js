214a4322084e93ae2b112b8ab2e0fddbcb4bc73d
berryb0y <ab4537@gmail.com>
2022-05-08 22:51:49 -0500

    security connection

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ const Models = require('./models');
 const Movies = Models.Movie;
 const Users = Models.User;
 
-mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true});
+// mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true});
+
+
 
 const { check, validationResult } = require('express-validator');
 
