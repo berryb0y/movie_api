4c6ef64467116ace48a1a5c6656b1c0056af787b
berryb0y <ab4537@gmail.com>
2022-05-16 20:17:48 -0500

    added console.log

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const Users = Models.User;
 
 // mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true});
-
+console.log('howdy');
 
 const { check, validationResult } = require('express-validator');
 
