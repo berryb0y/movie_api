c663fbc86fd5c91b1e29b65520b8109651768f8d
berryb0y <ab4537@gmail.com>
2022-05-26 19:24:45 -0500

    .

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234'];
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'localhost:1234'];
 app.use(cors({
     origin: (origin, callback) => {
         if(!origin) return callback(null, true);
